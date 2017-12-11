39f8ff795f5483e5e959655eac336004fee83e50
Nic Cope <negz@planet.com>
2017-12-10 16:34:13 -0800

    Add a script to run kubehook for local testing

---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+VERSION=$(git rev-parse --short HEAD)
+NAME=kubehook
+
+docker kill ${NAME} || true
+docker rm ${NAME} || true
+
+docker run -d \
+	--name ${NAME} \
+	-p 10003:10003 \
+	-e "KUBEHOOK_SECRET=secret" \
+	"negz/kubehook:${VERSION}" /kubehook/kubehook
