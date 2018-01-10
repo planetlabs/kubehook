1144ac41301c585fd964bce36d84597ab2d10527
Nic Cope <negz@planet.com>
2018-01-09 18:46:27 -0800

    Use bash rather than sh for helper scripts
    As I'm not sure sh has pushd/popd

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,8 +6,8 @@ ENDPOINT=$1
 TOKEN=$2
 
 curl -i -X GET \
-    -H "Content-Type: application/json" \
-    -d "{\"apiVersion\":\"authentication.k8s.io/v1beta1\",\"kind\":\"TokenReview\",\"spec\":{\"token\":\"${TOKEN}\"}}" \
-    "${ENDPOINT}"
+	-H "Content-Type: application/json" \
+	-d "{\"apiVersion\":\"authentication.k8s.io/v1beta1\",\"kind\":\"TokenReview\",\"spec\":{\"token\":\"${TOKEN}\"}}" \
+	"${ENDPOINT}"
 
 echo
