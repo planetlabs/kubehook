07b91c98147ff73b6fdad7b10effb64f7b7cb82e
Nic Cope <negz@planet.com>
2018-01-17 19:04:05 -0800

    Update scripts to support kubecfg endpoint

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,32 @@
 
 set -e
 
+ENABLE_TEMPLATE=$1
 VERSION=$(git rev-parse --short HEAD)
 NAME=kubehook
+
+CFG=$(mktemp -d /tmp/${NAME}.XXXX)
+cat <<EOF >$CFG/template
+apiVersion: v1
+kind: Config
+clusters:
+- name: kuberos
+  cluster:
+    certificate-authority-data: REDACTED
+    server: https://kuberos.example.org
+EOF
 
 docker kill ${NAME} || true
 docker rm ${NAME} || true
 
+KUBEHOOK_ARGS=""
+if [[ $ENABLE_TEMPLATE == "true" ]]; then
+	KUBEHOOK_ARGS="--kubecfg-template /cfg/template"
+fi
+
 docker run -d \
 	--name ${NAME} \
 	-p 10003:10003 \
+	-v $CFG:/cfg \
 	-e "KUBEHOOK_SECRET=secret" \
-	"negz/kubehook:${VERSION}" /kubehook
+	"negz/kubehook:${VERSION}" /kubehook $KUBEHOOK_ARGS
